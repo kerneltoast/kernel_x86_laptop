91763d25f4f5bc54bee45bdc7bdfc0b17534938e
Sultan Alsawaf <sultanxda@gmail.com>
2018-05-20 16:55:17 -0700

    Add Arch make/install/remove kernel scripts

---
+++ install-kernel-arch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo cp -v arch/x86_64/boot/bzImage /boot/vmlinuz-linux-sultan
+sudo cp -v System.map /boot/System.map-sultan
---
+++ update-grub-arch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo grub-mkconfig -o /boot/grub/grub.cfg
