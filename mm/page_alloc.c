9c13336ba45b2ff5c808c496b4ef556d155a9c29
Sultan Alsawaf <sultan@kerneltoast.com>
2021-10-20 20:50:32 -0700

    mm: Don't hog the CPU and zone lock in rmqueue_bulk()
    There is noticeable scheduling latency and heavy zone lock contention
    stemming from rmqueue_bulk's single hold of the zone lock while doing
    its work, as seen with the preemptoff tracer. There's no actual need for
    rmqueue_bulk() to hold the zone lock the entire time; it only does so
    for supposed efficiency. As such, we can relax the zone lock and even
    reschedule when IRQs are enabled in order to keep the scheduling delays
    and zone lock contention at bay. Forward progress is still guaranteed,
    as the zone lock can only be relaxed after page removal.
    
    With this change, rmqueue_bulk() no longer appears as a serious offender
    in the preemptoff tracer, and system latency is noticeably improved.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2230,14 +2230,15 @@ __rmqueue(struct zone *zone, unsigned int order, int migratetype,
 }
 
 /*
- * Obtain a specified number of elements from the buddy allocator, all under
- * a single hold of the lock, for efficiency.  Add them to the supplied list.
- * Returns the number of new pages which were placed at *list.
+ * Obtain a specified number of elements from the buddy allocator, and relax the
+ * zone lock when needed. Add them to the supplied list. Returns the number of
+ * new pages which were placed at *list.
  */
 static int rmqueue_bulk(struct zone *zone, unsigned int order,
 			unsigned long count, struct list_head *list,
 			int migratetype, unsigned int alloc_flags)
 {
+	const bool can_resched = !preempt_count() && !irqs_disabled();
 	unsigned long flags;
 	int i;
 
@@ -2247,6 +2248,15 @@ static int rmqueue_bulk(struct zone *zone, unsigned int order,
 								alloc_flags);
 		if (unlikely(page == NULL))
 			break;
+
+		/* Reschedule and ease the contention on the lock if needed */
+		if (i + 1 < count && ((can_resched && need_resched()) ||
+				      spin_needbreak(&zone->lock))) {
+			spin_unlock_irqrestore(&zone->lock, flags);
+			if (can_resched)
+				cond_resched();
+			spin_lock_irqsave(&zone->lock, flags);
+		}
 
 		/*
 		 * Split buddy pages returned by expand() are received here in
