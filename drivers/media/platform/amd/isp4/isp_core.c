edc7b8e6e1b0a0a52988c2caa6bfe1f89f960809
Sultan Alsawaf <sultan@kerneltoast.com>
2025-08-06 12:40:42 -0700

    media: amd: isp4: Add missing refcount tracking to mmap memop
    A reference on the buffer must be taken in the mmap memop in order to keep
    the backing mmap'd memory around. Add the missing refcount tracking pulled
    from vb2_vmalloc_mmap().
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/media/platform/amd/isp4/isp_core.c
+++ drivers/media/platform/amd/isp4/isp_core.c
@@ -216,6 +216,14 @@ static int vb2_amdisp_mmap(void *buf_priv, struct vm_area_struct *vma)
 	 */
 	vm_flags_set(vma, VM_DONTEXPAND);
 
+	/*
+	 * Use common vm_area operations to track buffer refcount.
+	 */
+	vma->vm_private_data	= &buf->handler;
+	vma->vm_ops		= &vb2_common_vm_ops;
+
+	vma->vm_ops->open(vma);
+
 	dev_dbg(buf->dev, "mmap isp user bo 0x%llx size %ld refcount %d",
 		buf->gpu_addr, buf->size, refcount_read(&buf->refcount));
 
